bd9e545a91b0574ee9846e3a16d77c04b3c7b3c0
Arthur Maia <arthur.maia.dev@gmail.com>
2020-12-22 23:29:40 -0300

    Start splash screen

---
+++ lib/screens/splash/components/body.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/size_config.dart';
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Welcome to Tokoto, Let's shop!",
+      "image": "assets/images/splash_1.png",
+    },
+    {
+      "text":
+          "We help people connect with store \naround United State of America",
+      "image": "assets/images/splash_2.png",
+    },
+    {
+      "text": "We show the easy way to shop. \nJust stay at home with us",
+      "image": "assets/images/splash_3.png",
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                itemBuilder: (context, index) => SplashContent(
+                  image: "assets/images/splash_1.png",
+                  text: "Welcome to Tokoto, Let's shop!",
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: SizedBox(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SplashContent extends StatelessWidget {
+  const SplashContent({
+    Key key,
+    this.text,
+    this.image,
+  }) : super(key: key);
+
+  final String text, image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Spacer(),
+        Text(
+          "TOKOTO",
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(36),
+            color: kPrimaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(text),
+        Spacer(flex: 2),
+        Image.asset(
+          image,
+          height: getProportionateScreenHeight(265),
+          width: getProportionateScreenWidth(235),
+        ),
+      ],
+    );
+  }
+}
