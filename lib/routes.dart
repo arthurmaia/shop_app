1cad13cc70b200a35fd131e11d0086cdd89ef14e
Arthur Maia <arthur.maia.dev@gmail.com>
2020-12-26 22:58:51 -0300

    Add SignIn screen to routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/widgets.dart';
 
 import './screens/splash/splash_screen.dart';
+import './screens/sign_in/sign_in_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
 };
