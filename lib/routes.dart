6b14d972f6e03ed614f753b3e14116993702a24a
Arthur Maia <arthur.maia.dev@gmail.com>
2021-01-02 19:52:01 -0300

    Add CompleteProfile screen to routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -5,6 +5,7 @@ import './screens/sign_in/sign_in_screen.dart';
 import './screens/sign_up/sign_up_screen.dart';
 import './screens/forgot_password/forgot_password_screen.dart';
 import './screens/login_success/login_success_screen.dart';
+import './screens/complete_profile/complete_profile_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
@@ -12,4 +13,5 @@ final Map<String, WidgetBuilder> routes = {
   ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
   LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
   SignUpScreen.routeName: (context) => SignUpScreen(),
+  CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
 };
